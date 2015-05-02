a9c3c50f806a4a2add035fd43567d6a906f2980e
user <user@server1.example.com>
2015-05-02 10:26:38 +0200

    modification readme.txt

--- web/app.py
+++ web/app.py
@@ -6,13 +6,12 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host="redis_1", port=6379)
 
-
 @app.route('/')
 def hello():
     redis.incr('views')
-    return 'Hello! This page has been seen {0} times.'.format(
+    return 'Bonjour! Cette page a été vue {0} fois.'.format(
         redis.get('views'))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
